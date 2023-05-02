9ce85ad7a8367fde3d0b5fa43ed529051508079b
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-05-02 01:48:30 +0000

    added card

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Navbar from "../component/Navbar.jsx";
+import Jumbotron from "../component/Jumbotron.jsx";
+import Card from "../component/Card.jsx";
 
 function Home(){
     return(
         <>
             <Navbar />
+            <div className="container">
+                <Jumbotron />
+                <Card />
+                
+
+            </div>
+            
         </>
     )
 }
