023698bcd66af173e4083a4116d0234be6e54523
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-05-05 00:10:26 +0000

    added styles

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 
-
 function Home(){
     const [counter, setCounter] = useState(0)
 
@@ -18,8 +17,9 @@ function Home(){
 
     return (
         <>
-            <div className="container">
+            <div className="container counter">
                 <div className="d-flex">
+                    <h1 className="w-25"><i class="fa-solid fa-clock fa-sm"></i></h1>
                     <h1 className="w-25">{Math.floor(counter / 100000) % 10 }</h1>
                     <h1 className="w-25">{Math.floor(counter / 10000) % 10 }</h1>
                     <h1 className="w-25">{Math.floor(counter / 1000) % 10 }</h1>
